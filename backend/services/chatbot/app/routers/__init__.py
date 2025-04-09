0dfbaf9aaabe197a95138bbc5c2e3e7e0ee8973a
Cuong Nguyen <cuong.nguyen@smartoptics.com>
2025-04-09 10:59:33 +0700

    cleanup code

--- backend/services/chatbot/app/routers/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from routers import chatbot_router
-from routers import health_router
-router = APIRouter()
-
-router.include_router(chatbot_router.router)
-router.include_router(health_router.router)
--- backend/services/document/app/routers/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from routers import document_router
-from routers import health_router
-router = APIRouter()
-
-router.include_router(document_router.router)
-router.include_router(health_router.router)
