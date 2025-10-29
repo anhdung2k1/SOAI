ceccd43e3b1509553258a8d433e58f52491bd4fd
Nhu Ngo <Nhu.Ngo@endava.com>
2025-10-29 15:50:37 +0700

    Create and integrate ReviewModal & ConfirmModal

--- web/recruitment-app/src/components/layouts/index.ts
+++ web/recruitment-app/src/components/layouts/index.ts
@@ -2,3 +2,5 @@ export { default as Container } from './Container';
 export { default as Row } from './Row';
 export { default as Col } from './Col';
 export { default as Badge } from './Badge';
+export { default as ConfirmModal } from './ConfirmModal';
+export { default as ReviewModal } from './ReviewModal';
